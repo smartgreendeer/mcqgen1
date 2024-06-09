fa1e8b107c1bb83e0321da34f84ab54df8bad92b
smartgreendeer <johnmuriithi7818@gmail.com>
2024-06-09 17:59:38 +0300

    streamlit

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -9,49 +9,54 @@ from langchain.callbacks import get_openai_callback
 from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
-
-
-with open('Response.json','r') as file:
+#loading json file
+with open('Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
-    
-st.title("MCQ GENERATOR APLLICATION WITH LANGCHAIN ")
 
-with st.form("user_inputs"):
-    uploaded_file=st.file_uploader("Upload a PDF or txt file")
-    
-    mcq_count=st.number_input("No.of MCQs", min_value=3, max_values=50)
-    
+
+#creating a title for the app
+st.title("John MCQs Creator Application with LangChain 🦜📚")
+
+
+with st.form("user input"):
+    uploaded_file=st.file_uploader("upload pdf or text")
+
+    mcq_count=st.number_input("no of mcq's", min_value=3, max_value=50)
+
     subject=st.text_input("Insert Subject",max_chars=20)
-    
-    tone=st.text_input("Complexity Level of Questions", max_chars=20, placeholder="simple")
-    
-    button=st.form_submit_button("create MCQs")
-    
+
+    tone=st.text_input("Complexity Level Of Questions", max_chars=20, placeholder="Simple")
+
+    button=st.form_submit_button("Create MCQs")
+
     if button and uploaded_file is not None and mcq_count and subject and tone:
-        with st.spinner("loading...."):
+        with st.spinner("loading..."):
             try:
                 text=read_file(uploaded_file)
+                #Count tokens and the cost of API call
                 with get_openai_callback() as cb:
                     response=generate_evaluate_chain(
                         {
-                            "text": text,
-                            "number": mcq_count,
-                            "subject": subject,
-                            "tone": tone,
-                            "response_json": json.dumps(RESPONSE_JSON)
-                        }
+                        "text": text,
+                        "number": mcq_count,
+                        "subject":subject,
+                        "tone": tone,
+                        "response_json": json.dumps(RESPONSE_JSON)
+                            }
                     )
-                    
+                #st.write(response)
+
             except Exception as e:
                 traceback.print_exception(type(e), e, e.__traceback__)
                 st.error("Error")
-                
+
             else:
                 print(f"Total Tokens:{cb.total_tokens}")
                 print(f"Prompt Tokens:{cb.prompt_tokens}")
                 print(f"Completion Tokens:{cb.completion_tokens}")
                 print(f"Total Cost:{cb.total_cost}")
                 if isinstance(response, dict):
+                    #Extract the quiz data from the response
                     quiz=response.get("quiz", None)
                     if quiz is not None:
                         table_data=get_table_data(quiz)
@@ -59,10 +64,10 @@ with st.form("user_inputs"):
                             df=pd.DataFrame(table_data)
                             df.index=df.index+1
                             st.table(df)
-                            
-                            st.text_area(labels="Review", value=response["review"])
+                            #Display the review in atext box as well
+                            st.text_area(label="Review", value=response["review"])
                         else:
-                            st.error("Failed to generate MCQs")
-                            
+                            st.error("Error in the table data")
+
                 else:
                     st.write(response)
--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -8,11 +8,10 @@ log_path=os.path.join(os.getcwd(),"logs")
 
 os.makedirs(log_path,exist_ok=True)
 
-
 LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
 
 
 logging.basicConfig(level=logging.INFO, 
-        filename=LOG_FILEPATH,
-        format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+                    filename=LOG_FILEPATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
